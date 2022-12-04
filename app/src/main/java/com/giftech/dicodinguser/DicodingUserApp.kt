93bd71ee641140a53591a0c860ae8584724add32
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-04 14:29:59 +0700

    create navhost

--- app/src/main/java/com/giftech/dicodinguser/DicodingUserApp.kt
+++ app/src/main/java/com/giftech/dicodinguser/DicodingUserApp.kt
@@ -1,8 +1,30 @@
 package com.giftech.dicodinguser
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.giftech.dicodinguser.ui.navigation.Screen
+import com.giftech.dicodinguser.ui.screens.home.HomeScreen
 
 @Composable
-fun DicodingUserApp() {
+fun DicodingUserApp(
+) {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = Modifier
+    ){
+        composable(Screen.Home.route){
+            HomeScreen()
+        }
+    }
 }
