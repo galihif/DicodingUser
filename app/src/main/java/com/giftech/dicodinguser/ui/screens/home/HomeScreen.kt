7b59534b9d52a2e063c71a35db9acb71629bc09b
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-04 17:36:17 +0700

    add content scale crop

--- app/src/main/java/com/giftech/dicodinguser/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/giftech/dicodinguser/ui/screens/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -66,6 +69,7 @@ fun HomeContent(
 ) {
     LazyColumn(
         modifier = Modifier,
+        contentPadding = PaddingValues(8.dp)
     ) {
         items(users) {
             UserItem(user = it) { id ->
@@ -84,9 +88,10 @@ fun UserItem(user: User, onClickItem: (Int) -> Unit) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         AsyncImage(
+            modifier = Modifier.width(64.dp).clip(CircleShape),
             model = user.imageUrl,
             contentDescription = null,
-            modifier = Modifier.width(64.dp)
+            contentScale = ContentScale.Crop,
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column(
