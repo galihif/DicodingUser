f43c42f86cb2d6ae461e2cee62f66b0cc40bd4cb
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-05 16:54:35 +0700

    adjust dummy data

--- app/src/main/java/com/giftech/dicodinguser/data/MainRepository.kt
+++ app/src/main/java/com/giftech/dicodinguser/data/MainRepository.kt
@@ -9,24 +9,73 @@ class MainRepository {
     val dummyUser = listOf(
         User(
             id = 1,
-            name = "John Doe",
+            name = "Samantha Taylor",
             imageUrl = "https://randomuser.me/api/portraits/women/1.jpg",
-            university = "University of California, Berkeley",
-            numCertificationsCompleted = 5
+            university = "Universitas Indonesia",
+            numCertificationsCompleted = 51
         ),
         User(
             id = 2,
-            name = "Jane Doe",
-            imageUrl = "https://randomuser.me/api/portraits/men/1.jpg",
-            university = "University of California, Los Angeles",
-            numCertificationsCompleted = 3
+            name = "Michael Johnson",
+            imageUrl = "https://randomuser.me/api/portraits/men/2.jpg",
+            university = "Institut Teknologi Bandung",
+            numCertificationsCompleted = 32
         ),
         User(
             id = 3,
-            name = "Alice Smith",
-            imageUrl = "https://i.pravatar.cc/150?img=1",
-            university = "Stanford University",
-            numCertificationsCompleted = 7
+            name = "Jessica Anderson",
+            imageUrl = "https://randomuser.me/api/portraits/women/3.jpg",
+            university = "Universitas Padjadjaran",
+            numCertificationsCompleted = 87
+        ),
+        User(
+            id = 4,
+            name = "David Williams",
+            imageUrl = "https://randomuser.me/api/portraits/men/4.jpg",
+            university = "Universitas Indonesia",
+            numCertificationsCompleted = 6
+        ),
+        User(
+            id = 5,
+            name = "Emily Thompson",
+            imageUrl = "https://randomuser.me/api/portraits/women/5.jpg",
+            university = "Institut Teknologi Bandung",
+            numCertificationsCompleted = 2
+        ),
+        User(
+            id = 6,
+            name = "Christopher Brown",
+            imageUrl = "https://randomuser.me/api/portraits/men/6.jpg",
+            university = "Universitas Diponegoro",
+            numCertificationsCompleted = 54
+        ),
+        User(
+            id = 7,
+            name = "Sarah Moore",
+            imageUrl = "https://randomuser.me/api/portraits/women/7.jpg",
+            university = "Universitas Gadjah Mada",
+            numCertificationsCompleted = 1
+        ),
+        User(
+            id = 8,
+            name = "John Davis",
+            imageUrl = "https://randomuser.me/api/portraits/men/8.jpg",
+            university = "Universitas Padjadjaran",
+            numCertificationsCompleted = 47
+        ),
+        User(
+            id = 9,
+            name = "Rachel Johnson",
+            imageUrl = "https://randomuser.me/api/portraits/women/9.jpg",
+            university = "Universitas Gadjah Mada",
+            numCertificationsCompleted = 23
+        ),
+        User(
+            id = 10,
+            name = "Matthew Thompson",
+            imageUrl = "https://randomuser.me/api/portraits/men/10.jpg",
+            university = "Universitas Diponegoro",
+            numCertificationsCompleted = 2
         )
     )
 
